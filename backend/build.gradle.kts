be0752fc9e8e0ad543e1b0e727ef565bd5a2601e
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2025-12-20 14:25:20 -0500

    chore: upgrade spring boot to 4.0.0

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
     kotlin("plugin.spring") version "2.0.21"
     kotlin("plugin.jpa") version "2.0.21"
-    id("org.springframework.boot") version "3.5.3"
+    id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
     alias(libs.plugins.kotlin.serialization)
     id("org.owasp.dependencycheck") version "11.1.1"
