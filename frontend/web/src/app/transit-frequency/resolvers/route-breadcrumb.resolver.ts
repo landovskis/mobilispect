3b18357eb4cc402c138eb928f186dcbb6344dd2a
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2025-12-20 16:12:30 -0500

    Refactor breadcrumbs to use Resolvers and add unit tests

---
+++ frontend/web/src/app/transit-frequency/resolvers/route-breadcrumb.resolver.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { FrequencyService } from '../services/frequency.service';
+
+@Injectable({ providedIn: 'root' })
+export class RouteBreadcrumbResolver implements Resolve<string> {
+    constructor(private readonly frequencyService: FrequencyService) { }
+
+    resolve(route: ActivatedRouteSnapshot): Observable<string> {
+        const routeId = route.paramMap.get('routeId');
+        if (!routeId) return of('Route');
+
+        return this.frequencyService.getRoute(routeId).pipe(
+            map(route => {
+                if (route.shortName && route.longName) {
+                    return `${route.shortName} ${route.longName}`;
+                }
+                return route.shortName || route.longName || routeId;
+            }),
+            catchError(() => of(routeId))
+        );
+    }
+}
