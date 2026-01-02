627167ac0d6b6d09261569ba0f1ccc1d44d2dc81
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2026-01-02 17:42:43 -0500

    fix: guard iOS room constructor

--- frontend/mobile/shared/src/iosMain/kotlin/com/mobilispect/mobile/data/AppDatabaseConstructor.kt
+++ frontend/mobile/shared/src/iosMain/kotlin/com/mobilispect/mobile/data/AppDatabaseConstructor.kt
@@ -3,5 +3,7 @@ package com.mobilispect.mobile.data
 import androidx.room.RoomDatabaseConstructor
 
 actual object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
-    actual override fun initialize(): AppDatabase = AppDatabase_Impl()
+    actual override fun initialize(): AppDatabase {
+        error("Room database is not supported on iOS targets.")
+    }
 }
