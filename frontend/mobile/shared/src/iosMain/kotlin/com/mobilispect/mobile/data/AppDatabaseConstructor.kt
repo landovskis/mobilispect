57777cff11ba65f84fc5d5e5520eeb41c6bf1018
Alex Landovskis <alex@MacBook-Pro.local>
2026-01-02 17:19:07 -0500

    chore: add mobile db constructor

---
+++ frontend/mobile/shared/src/iosMain/kotlin/com/mobilispect/mobile/data/AppDatabaseConstructor.kt
@@ -0,0 +1,7 @@
+package com.mobilispect.mobile.data
+
+import androidx.room.RoomDatabaseConstructor
+
+actual object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
+    override fun initialize(): AppDatabase = AppDatabase_Impl()
+}
