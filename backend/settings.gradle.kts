eb37db0048efab7bd7fb6ca93a64dfab02aa3c5f
Alex Landovskis <61091411+alandovskis@users.noreply.github.com>
2026-01-03 09:42:58 -0500

    chore(backend): update gradle wrapper

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,6 +1,4 @@
 rootProject.name = "mobilispect-backend"
 
-// Enable JDK auto-provisioning via Foojay Disco API
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
